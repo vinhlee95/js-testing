e36ca86eeb4d89db2914fc977c30d33ce434edfe
Vinh Le <vinh@vertics.co>
2019-12-25 09:03:08 +0200

    feat: add prettier configurations

--- static-testing/src/index.js
+++ static-testing/src/index.js
@@ -1,3 +1,8 @@
-const string = "abc";
+const string = 'abc'
 // typeof string === 'strng' eslint should complain about this
-typeof string === "string";
+typeof string === 'string'
+
+const myFunction = () => {
+	console.log('Hello world') // should have single quote
+	console.log('This line should have tab instead of space')
+}
