fbe9e9a252e42305970b7d8d2a0baaa08548a7fa
Vinh Le <vinh@vertics.co>
2019-12-25 19:01:45 +0200

    feat: add prettier and validate script

--- static-testing/src/index.js
+++ static-testing/src/index.js
@@ -4,5 +4,8 @@ typeof string === 'string'
 
 const myFunction = () => {
 	console.log('Hello world') // should have single quote
+
 	console.log('This line should have tab instead of space')
 }
+
+myFunction()
